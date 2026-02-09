4d1e7810fbca4cb0f7e5375f0bb1b41337b3bf12
Haruaki Tamada <tamada@users.noreply.github.com>
2026-02-09 12:45:29 +0900

    Enhance output logging for release dates and improve conditional checks in update_release_date script

--- resources/scripts/update_release_date.sh
+++ resources/scripts/update_release_date.sh
@@ -9,11 +9,15 @@ echo "========================================="
 gh api graphql -F query=@outputs/query.graphql > outputs/release_dates.json
 cat outputs/release_dates.json | jq -r "$(cat resources/scripts/json2csv.jq)" > outputs/release_dates.csv
 
+echo "=========== release_dates.csv ==========="
+cat outputs/release_dates.csv
+echo "========================================="
+
 cat outputs/release_dates.csv | while IFS=',' read -r repo_name tag_name publish_date url; do
   repo_name=$(echo ${repo_name} | tr -d \")
   publish_date=$(echo ${publish_date} | tr -d \")
   filepath=content/products/${repo_name}/index.md
-  if [[ -f ${filepath} ]] && [[ ${publish_date} != "N/A" ]]; then
+  if [ -f "${filepath}" ] && [ "${publish_date}" != "N/A" ]; then
     sed "s/^date: .*/date: ${publish_date}/" ${filepath} > a ; mv a ${filepath}
   fi
 done
