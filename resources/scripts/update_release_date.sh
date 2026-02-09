4855264a9348199bdd4c183dfe8401a409f27461
Haruaki Tamada <tamada@users.noreply.github.com>
2026-02-09 12:10:27 +0900

    Change shell interpreter to bash and improve date update logic in release date script

--- resources/scripts/update_release_date.sh
+++ resources/scripts/update_release_date.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 mkdir outputs
 bash resources/scripts/query_builder.sh > outputs/query.graphql
@@ -9,10 +9,11 @@ echo "========================================="
 gh api graphql -F query=@outputs/query.graphql > outputs/release_dates.json
 cat outputs/release_dates.json | jq -r "$(cat resources/scripts/json2csv.jq)" > outputs/release_dates.csv
 
-for line in $(cat outputs/release_dates.csv | tr -d \"); do
-  IFS=',' read -r -a items <<< "$line"
-  filepath=content/products/${items[0]}/index.md
-  if [[ -f ${filepath} ]] && [[ ${items[2]} != "N/A" ]]; then
-    sed "s/^date: .*/date: ${items[2]}/" ${filepath} >  a ; mv a ${filepath}
+tail -n +2 outputs/release_dates.csv | while IFS=',' read -r -a items; do
+  repo_name=$(echo ${items[0]} | tr -d \")
+  publish_date=$(echo ${items[2]} | tr -d \")
+  filepath=content/products/${repo_name}/index.md
+  if [[ -f ${filepath} ]] && [[ ${publish_date} != "N/A" ]]; then
+    sed -i "s/^date: .*/date: ${publish_date}/" ${filepath}
   fi
 done
